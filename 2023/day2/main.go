eed092046ab4673d1a0786702dd253b4096aea3a
matthias.rauline <matthias.rauline@epita.fr>
2023-12-07 01:01:53 +0100

    feat: add day 2

--- 2023/day2/main.go
+++ 2023/day2/main.go
@@ -1,7 +1,90 @@
 package day2
 
-func ParseDataSimple(data string) {
+import (
+	"strconv"
+	"strings"
+)
+
+func ParseDataSimple(data string) int {
+	splitted := strings.Split(data, "\n")
+
+	sum := 0
+	for _, line := range splitted {
+		isValid := true
+
+		linePart := strings.Split(line, ":")
+		gameInfo := strings.Split(linePart[0], " ")
+		id, _ := strconv.Atoi(gameInfo[1])
+
+		games := strings.Split(linePart[1], ";")
+		for _, game := range games {
+			cubes := strings.Split(game, ",")
+			for _, cube := range cubes {
+				cubeInfo := strings.Split(cube, " ")
+				number, _ := strconv.Atoi(cubeInfo[1])
+				color := cubeInfo[2]
+
+				switch color {
+				case "red":
+					if number > 12 {
+						isValid = false
+					}
+				case "green":
+					if number > 13 {
+						isValid = false
+					}
+				case "blue":
+					if number > 14 {
+						isValid = false
+					}
+				}
+			}
+		}
+
+		if isValid {
+			sum += id
+		}
+	}
+	return sum
 }
 
-func ParseDataHard(data string) {
+func ParseDataHard(data string) int {
+	splitted := strings.Split(data, "\n")
+
+	sum := 0
+	for _, line := range splitted {
+		maxRedCubes := 0
+		maxGreenCubes := 0
+		maxBlueCubes := 0
+
+		linePart := strings.Split(line, ":")
+
+		games := strings.Split(linePart[1], ";")
+		for _, game := range games {
+			cubes := strings.Split(game, ",")
+			for _, cube := range cubes {
+				cubeInfo := strings.Split(cube, " ")
+				number, _ := strconv.Atoi(cubeInfo[1])
+				color := cubeInfo[2]
+
+				switch color {
+				case "red":
+					if number > maxRedCubes {
+						maxRedCubes = number
+					}
+				case "green":
+					if number > maxGreenCubes {
+						maxGreenCubes = number
+					}
+				case "blue":
+					if number > maxBlueCubes {
+						maxBlueCubes = number
+					}
+				}
+			}
+		}
+
+		sum += (maxRedCubes * maxGreenCubes * maxBlueCubes)
+	}
+	return sum
 }
